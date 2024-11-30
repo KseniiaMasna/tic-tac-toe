507b1c28680bbbb74310356d4dec6f9291190d94
KseniiaMasna <kseniia.masna@gmail.com>
2024-11-30 20:12:53 +0100

    Implement logic to play against computer

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const message = document.querySelector('.message')
 
 const board = [];
-for (let i = 1; i < 10; i++) {
-  board.push(document.querySelector(`#c${i}`))
-}
 
-const freeCells = [];
-for (let i = 1; i < 10; i++) {
-    freeCells.push(i)
+//Function to generate board to track free cells to make move
+const generateBoard = () => {
+    for (let i = 1; i < 10; i++) {
+        board.push(document.querySelector(`#c${i}`))
+      }
 }
+generateBoard();
 
 
 const X = [];
@@ -26,19 +26,19 @@ const winCombinations = [
 ];
 
 //Function to pick random number for move
-const moveRandom = (freeCells) => {    
-    const index = Math.floor(Math.random() * (freeCells.length-1));    
-    console.log('Random index: ', index, 'Value of this index: ', freeCells[index])
-    return freeCells[index];
+const moveRandom = (board) => {    
+    const index = Math.floor(Math.random() * (board.length-1));    
+    console.log(board)
+    console.log('Random index: ', index, 'Value of this index: ', board[index].id)
+    return board[index];
 }
 
 //Function to remove cell from free cells
-const bookCell = (freeCells, cellToRemove) => {
-    let index = freeCells.indexOf(cellToRemove);
+const bookCell = (border, cellToRemove) => {
+    let index = border.indexOf(cellToRemove);
     if (index !== -1) { 
-        freeCells.splice(index, 1);
-      }
-    console.log(freeCells)
+        border.splice(index, 1);
+      }    
 }
 
 
@@ -53,11 +53,11 @@ const checkWin = (X, winCombinations) => {
 const checkMove = (cell) => {
     if(move % 2 !== 0){
         cell.innerText = 'X' 
-        X.push(board.indexOf(cell));
+        X.push(parseInt(cell.id.slice(1), 10)-1);
         return checkWin(X, winCombinations)
     } else if (move % 2 === 0){
         cell.innerText = 'O' 
-        O.push(board.indexOf(cell));
+        O.push(parseInt(cell.id.slice(1), 10)-1);
         return checkWin(O, winCombinations)
     } else {
         console.log('Error')
@@ -65,56 +65,53 @@ const checkMove = (cell) => {
 }
 
 //Function to reset data to restart the game
-const resetData = () => {
-    board.forEach(cell =>{
+const resetData = () => {  
+    const cells = document.querySelectorAll('.cell');    
+    cells.forEach(cell => {
         cell.innerText = ''
-        message.innerText = 'The restart button is over here...';          
-    })        
-    for (let i = 1; i < 10; i++) {
-        freeCells.push(i)
-    }
+        
+    })  
+    message.innerText = 'The restart button is over here...';                
     X.length = 0;  
     O.length = 0;    
     move = 0;   
+    generateBoard();
+}
+
+//Function to make move
+const makeMove = (cell) => {
+    move++
+    if (checkMove(cell)){                
+        if (move % 2 !== 0){
+            message.innerText = 'Game over. X won!'
+            return
+        } else if (move % 2 === 0){
+            message.innerText = 'Game over. O won!'
+            return
+        }
+    } else if (move === 9){
+        message.innerText = 'Draw!'
+        return
+    }
+
 }
 
 
 //Initialization the game
 document.addEventListener('click', e => {
     board.forEach(cell =>{
-        if(e.target.matches(`#${cell.id}`)){            
-            move ++;
-            bookCell(freeCells, parseInt(cell.id.slice(1), 10))
-            if (checkMove(cell)){                
-                if (move % 2 !== 0){
-                    message.innerText = 'Game over. X won!'
-                    return
-                } else if (move % 2 === 0){
-                    message.innerText = 'Game over. O won!'
-                    return
-                }
-            } else if (move === 9){
-                message.innerText = 'Draw!'
-                return
-            }
-
+        if(e.target.matches(`#${cell.id}`)){                    
+            //my move
+            bookCell(board, cell)
+            makeMove(cell)
+            
+            
+            
             //computer move
-            let moveComputer = moveRandom(freeCells)        
-            let cellComputer = document.querySelector(`#c${moveComputer}`)
-            move ++;
-            bookCell(freeCells, moveComputer)  
-            if (checkMove(cellComputer)){                
-                if (move % 2 !== 0){
-                    message.innerText = 'Game over. X won!'
-                    return
-                } else if (move % 2 === 0){
-                    message.innerText = 'Game over. O won!'
-                    return
-                }
-            } else if (move === 9){
-                message.innerText = 'Draw!'
-                return
-            }            
+            let cellComputer = moveRandom(board)                       
+            
+            bookCell(board, cellComputer)  
+            makeMove(cellComputer)  
         }
     })
     if(e.target.matches('button')){
